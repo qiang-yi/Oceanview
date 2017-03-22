c1fbb0bbe571d93880916e2448514da71c24bad1
yi <yi@128.171.9.143>
2017-03-21 16:18:28 -1000

    new update

--- dist_obs_pts.py
+++ dist_obs_pts.py
@@ -16,25 +16,25 @@ arcpy.CheckOutExtension("Spatial")
 
 arcpy.env.overwriteOutput=True
 
-point_dir="C:/Users/Yi Qiang/Documents/UH_work/oceanview/obs_points/"
-points_20k="C:/Users/Yi Qiang/Documents/UH_work/oceanview/other_data/sample_point_20k.shp"
-dem_20k="C:/Users/Yi Qiang/Documents/UH_work/oceanview/other_data/dem_20k"
-#dem_20k="C:/Users/Yi Qiang/Documents/UH_work/oceanview/LiDAR_dem/dsm_20k"
-output_dir="C:/Users/Yi Qiang/Documents/UH_work/oceanview/dist_obs_points/"
-#output_dir="C:/Users/Yi Qiang/Documents/UH_work/oceanview/viewshed_dsm/"
-origin_dem="C:/Users/Yi Qiang/Documents/UH_work/oceanview/Oahu_DEM_10m/dem_10m"
-#origin_dem="C:/Users/Yi Qiang/Documents/UH_work/oceanview/LiDAR_dem/dsm"
+point_dir="D:/oceanview/obs_points/"
+points_20k="D:/oceanview/other_data/sample_point_20k_2.shp"
+dem_20k="D:/oceanview/LiDAR_dem/dsm_20k"
+#dem_20k="D:/UH_work/oceanview/LiDAR_dem/dsm_20k"
+output_dir="D:/oceanview/dist_obs_points/"
+#output_dir="D:/UH_work/oceanview/viewshed_dsm/"
+origin_dem="D:/oceanview/LiDAR_dem/dsm"
+#origin_dem="D:/UH_work/oceanview/LiDAR_dem/dsm"
 
 arcpy.env.extent =dem_20k
 arcpy.env.snapRaster = dem_20k
 #arcpy.env.mask = dem_20k
 
-for row in arcpy.da.SearchCursor(points_20k, ["SHAPE@","RASTERVALU","FID"]):
+for row in arcpy.da.SearchCursor(points_20k, ["SHAPE@","FID"]):
     # Print x,y coordinates of each point feature
 
-    fid=row[2]
+    fid=row[1]
     
-    if fid >= 83 and fid<84:
+    if fid >= 316 and fid<400:
         start_time=time.time()
         pt_file=point_dir+str(fid)+".shp"
         dist_file=output_dir+"dist_"+str(fid)
--- viewshed.py
+++ viewshed.py
@@ -15,19 +15,21 @@ arcpy.CheckOutExtension("Spatial")
 
 arcpy.env.overwriteOutput=True
 
-point_dir="C:/Users/Yi Qiang/Documents/UH_work/oceanview/obs_points/"
-points_20k="C:/Users/Yi Qiang/Documents/UH_work/oceanview/other_data/sample_point_20k_2.shp"
-dem_20k="C:/Users/Yi Qiang/Documents/UH_work/oceanview/LiDAR_dem/dsm_20k_int"
-#dem_20k="C:/Users/Yi Qiang/Documents/UH_work/oceanview/LiDAR_dem/dsm_20k"
-output_dir="C:/Users/Yi Qiang/Documents/UH_work/oceanview/viewshed_dsm/"
-output_dist_dir="C:/Users/Yi Qiang/Documents/UH_work/oceanview/dist_obs_points/"
-#output_dir="C:/Users/Yi Qiang/Documents/UH_work/oceanview/viewshed_dsm/"
-origin_dem="C:/Users/Yi Qiang/Documents/UH_work/oceanview/LiDAR_dem/dsm_20k_int"
-#origin_dem="C:/Users/Yi Qiang/Documents/UH_work/oceanview/LiDAR_dem/dsm"
+point_dir="D:/oceanview/obs_points/"
+points_20k="D:/oceanview/other_data/sample_point_20k_2.shp"
+dem_20k="D:/oceanview/LiDAR_dem/dtm_20k_int"
+#dem_20k="D:/oceanview/LiDAR_dem/dtm_20k"
+output_dir="D:/oceanview/viewshed_dtm/"
+output_dist_dir="D:/oceanview/dist_obs_points/"
+#output_dir="D:/oceanview/viewshed_dtm/"
+origin_dem="D:/oceanview/LiDAR_dem/dtm_20k_int"
+#origin_dem="D:/oceanview/LiDAR_dem/dtm"
 
 arcpy.env.extent =dem_20k
 arcpy.env.snapRaster = dem_20k
 #arcpy.env.mask = dem_20k
+arcpy.env.overwriteOutput=True
+
 
 for row in arcpy.da.SearchCursor(points_20k, ["SHAPE@","FID"]):
     # Print x,y coordinates of each point feature
@@ -36,10 +38,10 @@ for row in arcpy.da.SearchCursor(points_20k, ["SHAPE@","FID"]):
     #dist=row[1]
     fid=row[1]
     
-    if fid >= 94 and fid<100:
+    if fid >= 240 and fid<241:
         start_time = time.time()
         fname=str(fid)+'.shp'
-        pnt=arcpy.CreateFeatureclass_management(point_dir, fname, "POINT", "C:/Users/Yi Qiang/Documents/UH_work/oceanview/other_data/sea_point.shp", "", "", origin_dem)
+        pnt=arcpy.CreateFeatureclass_management(point_dir, fname, "POINT", "D:/oceanview/other_data/sea_point.shp", "", "", origin_dem)
         arcpy.AddField_management(pnt, "Dist_Coast", "FLOAT")
         cursor = arcpy.da.InsertCursor(pnt, ["SHAPE@"])
         cursor.insertRow([pnt_obj])
